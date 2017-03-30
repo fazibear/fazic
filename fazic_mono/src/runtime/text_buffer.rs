67055918f4301949d998be196d2be34c5659a71a
Michał Kalbarczyk <fazibear@gmail.com>
2017-03-30 17:13:52 +0200

    Move cursor

--- fazic_mono/src/runtime/text_buffer.rs
+++ fazic_mono/src/runtime/text_buffer.rs
@@ -1,6 +1,6 @@
 pub struct TextBuffer {
     pub string: String,
-    pub colors: [i32; 1040],
+    pub colors: [i32; 1000],
     pub cursor: usize,
 }
 
@@ -8,30 +8,45 @@ impl TextBuffer {
     pub fn new() -> TextBuffer {
         TextBuffer {
             string: "                                                     **** FAZIC ****                                                    READY.".to_string(),
-            colors: [5; 1040],
+            colors: [5; 1000],
             cursor: 160,
         }
     }
 
     pub fn left(&mut self) {
-        self.cursor -= 1
+        if self.cursor.checked_sub(1).is_some() {
+            self.cursor -= 1;
+        }
     }
 
     pub fn right(&mut self) {
-        self.cursor += 1
+        if self.cursor + 1 <= 999 {
+            self.cursor += 1;
+        } else {
+            self.cursor -= 39;
+            self.shift();
+        }
     }
 
     pub fn up(&mut self) {
-        self.cursor -= 40;
-        if self.cursor <= 0 {
-            self.cursor = 0;
+        if self.cursor.checked_sub(40).is_some() {
+            self.cursor -= 40;
         }
     }
 
     pub fn down(&mut self) {
-        self.cursor += 40;
-        if self.cursor > 1040 {
-            self.cursor = 1040;
+        if self.cursor + 40 < 999 {
+            self.cursor += 40;
+        } else {
+            self.shift();
+        }
+    }
+
+    pub fn shift(&mut self) {
+        if self.string.len() > 40 {
+            self.string = self.string[40..self.string.len()].to_string();
+        } else {
+            self.string = "".to_string();
         }
     }
 }
