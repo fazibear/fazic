08d6eb0acde449340e94b53fc116514801efd141
Michał Kalbarczyk <fazibear@gmail.com>
2017-04-09 18:40:14 +0200

    Add load indicator

--- fazic_mono/static/index.js
+++ fazic_mono/static/index.js
@@ -2,4 +2,8 @@ var Module = {
   print: function(text) { console.log(text) },
   printErr: function(text) { console.error(text) },
   canvas: document.getElementById('canvas'),
+  setStatus: function(status) {
+    document.getElementById('status').innerHTML = status;
+  }
 };
+Module.setStatus('Downloading...');
