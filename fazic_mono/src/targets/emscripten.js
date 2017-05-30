063b7baacb93e20a7a763104b2050eaacafaa38c
Michał Kalbarczyk <fazibear@gmail.com>
2017-05-30 14:20:16 +0200

    Fix connection errors

--- fazic_mono/src/targets/emscripten.js
+++ fazic_mono/src/targets/emscripten.js
@@ -1,22 +1,26 @@
 mergeInto(LibraryManager.library, {
   js_fetch: function(url_p, method_p, data_p, code_p, resp_p, size_p) {
-    url = Pointer_stringify(url_p);
-    method = Pointer_stringify(method_p);
-    data = Pointer_stringify(data_p) || null;
+    var url = Pointer_stringify(url_p);
+    var method = Pointer_stringify(method_p);
+    var data = Pointer_stringify(data_p) || null;
 
-    var request = new XMLHttpRequest();
-    request.open(method, url, false);
-    request.send(data);
+    var code, response;
 
-    var str = request.responseText;
-    var code = request.status;
+    try {
+      var request = new XMLHttpRequest();
+      request.open(method, url, false);
+      request.send(data);
+      code = request.status;
+      response = request.responseText;
+    } catch(err) {
+      code = 0;
+      response = "CAN'T CONNECT"
+    }
 
-    var size = new Blob([str]).size + 1;
-    stringToUTF8(str, resp_p, size);
+    var size = new Blob([response]).size + 1;
+    stringToUTF8(response, resp_p, size);
 
     setValue(size_p, size, 'i32');
     setValue(code_p, code, 'i32');
-
-    console.log(str);
   },
 });
