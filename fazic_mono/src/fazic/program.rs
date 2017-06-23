3229513420a502cf3a8f7bc23f3b73e35bdacc87
Michał Kalbarczyk <fazibear@gmail.com>
2017-06-23 20:51:09 +0200

    Cleanup program struct

--- fazic_mono/src/fazic/program.rs
+++ fazic_mono/src/fazic/program.rs
@@ -1,23 +1,19 @@
 use fazic::ast::*;
-use fazic::config::*;
 
 pub struct Program {
-    pub lines: Vec<(u16, String)>,
-    pub ast: Vec<Vec<NodeElement>>,
+    pub lines: Vec<(u16, String, Vec<NodeElement>)>,
 }
 
 impl Program {
     pub fn new() -> Program {
         Program {
             lines: vec![],
-            ast: vec![vec![]; BASIC_MAX_LINES as usize],
         }
     }
 
     pub fn add_line(&mut self, line: u16, nodes: Vec<NodeElement>, string: String) {
-        self.lines.retain(|&(l, _)| l != line);
-        self.lines.push((line, string));
-        self.lines.sort_by(|&(a, _), &(b, _)| a.cmp(&b));
-        self.ast[line as usize] = nodes;
+        self.lines.retain(|&(l, _, _)| l != line);
+        self.lines.push((line, string, nodes));
+        self.lines.sort_by(|&(a, _, _), &(b, _, _),| a.cmp(&b));
     }
 }
