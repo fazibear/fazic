8900f927124e3d79d5af19184ebdf62cbb4b2066
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-16 18:28:45 +0100

    Add about page

--- fazic_static/app/components/about.rb
+++ fazic_static/app/components/about.rb
@@ -3,6 +3,11 @@ class About
 
   def render
     h3 "What is this?"
-    div "duapsofsdog dfjg sdfgsd"
+    p "FAZIC is a fantasy retro computer. You can create, share, and play tiny games or programs. You have build in BASIC interpreter, so you can start now like it's the 80s."
+    h3 do
+      a href: "/fazic" do
+        'Click here to start'
+      end
+    end
   end
 end
