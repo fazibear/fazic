3eb8b0c4795fe41c96ee3dbf66ed2e67063f4e3b
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-16 19:27:50 +0100

    Add roadmap

--- app/components/roadmap.rb
+++ app/components/roadmap.rb
@@ -3,5 +3,28 @@ class Roadmap
 
   def render
     h3 "Things to do!"
+    h4 "BASIC"
+    ul do
+      li "Graphic"
+      li "Sprites"
+      li "Sound"
+      li "Arrays"
+      li "Hashes"
+      li "Networking"
+    end
+    h4 "Infrastructure"
+    ul do
+      li "Save in the cloud"
+      li "Share programs"
+      li "Load other user programs"
+    end
+    h4 "Native clients"
+    ul do
+      li "OSX"
+      li "Windows"
+      li "Linux"
+      li "iOS"
+      li "Android"
+    end
   end
 end
