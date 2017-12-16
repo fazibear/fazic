b42eec98f06de0b24fe0d7b79e19ec71a0a0c5e5
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-16 19:56:32 +0100

    Update roadmap

--- fazic_static/app/components/roadmap.rb
+++ fazic_static/app/components/roadmap.rb
@@ -5,18 +5,21 @@ class Roadmap
     h3 "Things to do!"
     h4 "BASIC"
     ul do
-      li "Graphic"
-      li "Sprites"
+      li "User Input Commands"
+      li "Graphic Commands"
+      li "Sprites Support"
       li "Sound"
-      li "Arrays"
-      li "Hashes"
-      li "Networking"
+      li "Arrays Support"
+      li "Hashes Support"
+      li "Networking Support"
     end
     h4 "Infrastructure"
     ul do
-      li "Save in the cloud"
-      li "Share programs"
-      li "Load other user programs"
+      li "Upload you programs"
+      li "Save programs in the cloud"
+      li "Share programs with other clients"
+      li "Share programs with other users"
+      li "Load other users programs"
     end
     h4 "Native clients"
     ul do
