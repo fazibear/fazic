7147505eea37decfa8c35ec347b4ccbdc5d4d0c2
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-25 20:00:18 +0100

    Rename programs to disc

--- fazic_static/app/router.rb
+++ fazic_static/app/router.rb
@@ -4,7 +4,7 @@ class Router
   def routes
     route '/', to: About
     route '/help', to: Help
-    route '/programs', to: Programs
+    route '/disc', to: Disc
     route '/roadmap', to: Roadmap
     route '/patreons', to: Patreons
   end
