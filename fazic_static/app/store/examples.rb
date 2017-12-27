b016c5fe8ec2aec1f1efe06038cb475f47e92d96
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-27 17:56:22 +0100

    Add example program

--- fazic_static/app/store/examples.rb
+++ fazic_static/app/store/examples.rb
@@ -2,10 +2,27 @@ class Store
   module Examples
     EXAMPLES = [
       {
-        name: "HelloWorld",
-        description: "Evaluates to the absolute value (value without the sign) of the given numeric term.",
+        name: "HELLO",
+        description: "Simple hello wrold basic program",
         code: <<~EOP
           10 PRINT "Hello"
+          20 GOTO 10
+        EOP
+      },
+      {
+        name: "COLORS",
+        description: "Fill all screen with pixel and show it.",
+        code: <<~EOP
+          5 mode 1
+          10 for c=0 to 15
+          20 for y=0 to 240
+          30 for x=0 to 320
+          40 dot x,y
+          50 next
+          60 next
+          70 flip
+          80 color c
+          90 next
         EOP
       },
     ]
