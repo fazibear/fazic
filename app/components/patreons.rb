c772e550a7cd115595fae20c01412809188ad579
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-27 17:27:44 +0100

    Add patreon and itch.io page

--- app/components/patreons.rb
+++ app/components/patreons.rb
@@ -2,6 +2,18 @@ class Patreons
   include Inesita::Component
 
   def render
-    h3 "Patreons"
+    h3 "Patreon"
+    p "If you like it. Want more. You can support me on patreon!"
+    a href: "https://www.patreon.com/fazic" do
+      "https://www.patreon.com/fazic"
+    end
+    p "Thanks!"
+    h3 "itch.io"
+    p "You can also check out itch.io page."
+    a href: "https://fazibear.itch.io/fazic" do
+      "https://fazibear.itch.io/fazic"
+    end
+    p "Thanks!"
+    h3 "Who support this project?"
   end
 end
